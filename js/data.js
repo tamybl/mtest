3744a9da484aff7a0a2899ac57ed4792152bc3ed
tamybl <tamara.bravo.l@gmail.com>
2019-02-07 16:11:15 -0300

    últimas modificaciones

--- js/data.js
+++ js/data.js
@@ -1,77 +1,76 @@
 document.addEventListener('DOMContentLoaded', () => {
-const contentPosts = $('#allposts');
+    const contentPosts = $('#allposts');
 
-const urls = [
-    'https://jsonplaceholder.typicode.com/posts',
-    'https://jsonplaceholder.typicode.com/photos'
-];
+    const urls = [
+        'https://jsonplaceholder.typicode.com/posts',
+        'https://jsonplaceholder.typicode.com/photos'
+    ];
 
-// Fetch 
-Promise.all(urls.map(url =>
-    fetch(url)
-        .then(checkStatus)
-        .then(res => res.json())
-        .catch(error => console.log('Ups!, hay un problema', error))
-)).then(data => {
+    const promesas = [];
 
-    generatePosts(data[0]);
-});
-
-function checkStatus(response) {
-    if (response.ok) {
-        return Promise.resolve(response);
-    } else {
-        return Promise.reject(new Error(response.statusText));
+    // Creando promise
+    function createPromise(url) {
+        return new Promise((resolve, reject) => {
+            fetch(url)
+                .then(res => resolve(res.json()))
+                .catch(error => reject(error))
+        })
     }
-}
 
-function generatePosts(posts) {
-    posts.map((post) => {
-        let article = createNode('article'),
-            h2 = createNode('h2'),
-            p = createNode('p'),
-            span = createNode('span');
+    urls.map(url => promesas.push(createPromise(url)));
 
-        article.setAttribute('id', post.id);
-        article.classList.add('post--item');
-        span.classList.add('post_date--item');
-        span.innerHTML = `Autor: ${post.userId}`;
-        p.innerHTML = `${post.body}`;
-        h2.innerHTML = `${post.title}`;
-
-        fetch('https://jsonplaceholder.typicode.com/photos/' + post.id)
-            .then(res => res.json())
-            .then((json) => {
-                let images = json;
-                let div = createNode('div')
-                img = createNode('img');
-                img.src = images.thumbnailUrl;
-                img.alt = images.title;
-                img.classList.add('photo-thumb');
-                append(div, img);
-                append(article, div);
-            })
-            .catch(error => console.log('Ups!, hay un problema', error));
-
-        append(article, h2);
-        append(article, span);
-        append(article, p);
-        append(contentPosts, article);
+    Promise.all(promesas).then(values => {
+        generatePosts(values);
     })
 
-}
 
-function $(element) {
-    return document.querySelector(element);
-}
+    function generatePosts(posts) {
+        posts[0].map((text, index) => {
 
-function createNode(element) {
-    return document.createElement(element);
-}
+            let article = createNode('article'),
+                h2 = createNode('h2'),
+                p = createNode('p'),
+                span = createNode('span'),
+                div = createNode('div'),
+                img = createNode('img');
 
-function append(parent, child) {
-    return parent.appendChild(child);
-}
+            article.setAttribute('id', text.id);
+            article.classList.add('post-item');
+            span.classList.add('post_date--item');
+            span.innerHTML = `Autor: ${text.userId}`;
+            p.classList.add('post_content--text');
+            p.innerHTML = `${text.body}`;
+            h2.innerHTML = `${text.title}`;
 
- })
+            // Nodo Imagenes
+            img.src = posts[1][index].thumbnailUrl;
+            img.alt = posts[1][index].title;
+            img.classList.add('photo-thumb');
+            append(div, img);
 
+
+            // Agregando contenido a article
+            append(article, h2);
+            append(article, span);
+            append(article, p);
+            append(article, div);
+
+            append(contentPosts, article);
+
+        })
+
+    }
+
+    function $(element) {
+        return document.querySelector(element);
+    }
+
+    function createNode(element) {
+        return document.createElement(element);
+    }
+
+    function append(parent, child) {
+        return parent.appendChild(child);
+    }
+
+})
